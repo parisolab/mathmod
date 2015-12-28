a888ce1373854d46cd141f2325b0f8a58b030c2b
Abderrahman Taha <taha_ab@yahoo.fr>
2015-12-28 23:47:58 +0000

    Code cleaning and optimization
    MathMod - https://sourceforge.net/p/mathmod/tickets/2

--- ui_modules/ParisoMathObject.cpp
+++ ui_modules/ParisoMathObject.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2015 by Abderrahman Taha                                *
+ *   Copyright (C) 2016 by Abderrahman Taha                                *
  *                                                                         *
  *                                                                         *
  *   This program is free software;you can redistribute it and/or modify  *
