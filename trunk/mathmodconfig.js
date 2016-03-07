8ebc08b88d07f888688334fb3449c38e580a3057
virtualmeet <virtualmeet@28a82157-60eb-4bc3-850b-3112dc87c63f>
2016-03-07 07:59:24 +0000

    Support of Comma (,) and Point (.) symbols for decimal numbers MathMod - https://sourceforge.net/p/mathmod/tickets/63

--- trunk/mathmodconfig.js
+++ trunk/mathmodconfig.js
@@ -1,8 +1,8 @@
 {
     "IsoParam": {
-        "MaxGrid": 150,
-        "MaxPt": 2,
-        "MaxTri": 3
+        "MaxGrid": 250,
+        "MaxPt": 5,
+        "MaxTri": 8
     },
     "WindowPosition": {
         "ControlX": 20,
@@ -10,35 +10,57 @@
         "GlwinX": 580,
         "GlwinY": 20
     },
-    "WindowSize": {
-        "ControlH": 700,
-        "ControlW": 538,
-        "GlwinH": 700,
-        "GlwinW": 780
+"WindowSize":{"ControlH":700,"ControlW":538,"GlwinH":700,"GlwinW":780
     },
-
     "Styles": {
-        "StylesPredefined": [ "Default", "Fusion", "Windows","WindowsXp"],
-        "StyleUsed": "Default"
+        "StylesPredefined": ["Default",
+        "Fusion",
+        "Windows",
+        "WindowsXp"],
+        "StyleUsed": "Fusion"
     },
-
     "Themes": {
-        "ThemesPredefined": [ "Default", "Dark", "MyTheme"],
-        "ThemeUsed": "Default",
+        "ThemesPredefined": ["Default",
+        "Dark",
+        "MyTheme"],
+        "ThemeUsed": "Dark",
         "MyTheme": {
-            "Window":[53,53,53],
-            "WindowText":[255, 255, 255],
-            "Base":[15,15,15],
-            "AlternateBase":[53,53,53],
-            "ToolTipBase":[255, 255, 255],
-            "ToolTipText":[255, 255, 255],
-            "Text":[2, 2, 255],
-            "Button":[53,53,53],
-            "ButtonText":[255, 255, 255],
-            "BrightText":[255, 0, 0],
-            "Highlight":[142,45,197],
-            "HighlightedText":[0, 0, 0]
+            "Window": [53,
+            53,
+            53],
+            "WindowText": [255,
+            255,
+            255],
+            "Base": [15,
+            15,
+            15],
+            "AlternateBase": [53,
+            53,
+            53],
+            "ToolTipBase": [255,
+            255,
+            255],
+            "ToolTipText": [255,
+            255,
+            255],
+            "Text": [2,
+            2,
+            255],
+            "Button": [53,
+            53,
+            53],
+            "ButtonText": [255,
+            255,
+            255],
+            "BrightText": [255,
+            0,
+            0],
+            "Highlight": [142,
+            45,
+            197],
+            "HighlightedText": [0,
+            0,
+            0]
         }
     }
-
-    }
+}
