ab3dbbd8524b65329031081bbcfa656747f74fdc
Abderrahman Taha <taha_ab@yahoo.fr>
2020-02-07 21:19:41 -0500

    #11 Code cleaning and optimization

--- pariso/isosurface/Iso3D.h
+++ pariso/isosurface/Iso3D.h
@@ -1,6 +1,6 @@
 
 /***************************************************************************
- *   Copyright (C) 2020by Abderrahman Taha                                *
+ *   Copyright (C) 2020 by Abderrahman Taha                                *
  *                                                                         *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
--- ui_modules/ParisoMathObject.cpp
+++ ui_modules/ParisoMathObject.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2020by Abderrahman Taha                                *
+ *   Copyright (C) 2020 by Abderrahman Taha                                *
  *                                                                         *
  *                                                                         *
  *   This program is free software;you can redistribute it and/or modify  *
