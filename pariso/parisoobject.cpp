9b3c3b970c6ee031858d6147d8c2e5b31187ec4a
Abderrahman Taha <taha_ab@yahoo.fr>
2020-10-23 17:12:01 -0400

    #11 Code cleaning and optimization

--- pariso/parisoobject.cpp
+++ pariso/parisoobject.cpp
@@ -19,6 +19,7 @@
  ***************************************************************************/
 #include "parisoobject.h"
 
+
 MasterThread::MasterThread() {}
 MasterThread::~MasterThread() {}
 WorkerThread::WorkerThread() {}
