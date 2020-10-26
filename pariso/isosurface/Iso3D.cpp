bc743bf09ffee543cc66e3f6100f5da4f9bcdd43
Abderrahman Taha <taha_ab@yahoo.fr>
2020-10-25 23:51:41 -0400

    #11 Code cleaning and optimization

--- pariso/isosurface/Iso3D.cpp
+++ pariso/isosurface/Iso3D.cpp
@@ -29,11 +29,9 @@ static std::vector<float> NormOriginaltmpVector;
 uint NbTriangleIsoSurface,NbPointIsoMap;
 uint OrignbX, OrignbY, OrignbZ;
 uint Stack_Factor=OrignbX*OrignbY*OrignbZ;
-
 std::vector<float> NormVertexTabVector;
 std::vector<uint>  IndexPolyTabMinVector;
 std::vector<uint>  IndexPolyTabVector;
-
 static CellNoise *NoiseFunction = new CellNoise();
 static ImprovedNoise *PNoise = new ImprovedNoise(4., 4., 4.);
 static QElapsedTimer times;
