8f4f47e607cf97be85199a685328a8f2db1eec4c
Abderrahman Taha <taha_ab@yahoo.fr>
2021-05-28 03:32:48 -0400

    #193 Add support for 3D textures/colors based on complex parameters

--- pariso/commun.h
+++ pariso/commun.h
@@ -25,19 +25,6 @@
 #define PI (double(314159265) / double(100000000))
 #define MAGIC_SCALE 1.5707963f
 extern template class FunctionParserBase<double>; // explicit instantiation
-// declaration
-typedef struct {
-    double r;       // a fraction between 0 and 1
-    double g;       // a fraction between 0 and 1
-    double b;       // a fraction between 0 and 1
-} rgb;
-
-typedef struct {
-    double h;       // angle in degrees
-    double s;       // a fraction between 0 and 1
-    double v;       // a fraction between 0 and 1
-} hsv;
-
 
 struct GlobalParam
 {
