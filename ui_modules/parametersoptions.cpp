88166bfdc385bff45b138ced1fac83bdc7123dc3
Abderrahman Taha <taha_ab@yahoo.fr>
2023-01-09 22:44:06 -0500

    #44 Scripts cleaning and optimization

--- ui_modules/parametersoptions.cpp
+++ ui_modules/parametersoptions.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2022 by Abderrahman Taha                                *
+ *   Copyright (C) 2023 by Abderrahman Taha                                *
  *                                                                         *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
