db5c7d6b4fa4f04e5fe913132e117b2d8d8b25e5
Abderrahman Taha <taha_ab@yahoo.fr>
2023-12-12 05:50:21 -0500

    #180 Code cleaning

--- pariso/parametric/ND/Matrix4D.h
+++ pariso/parametric/ND/Matrix4D.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2023 by Abderrahman Taha                                *
+ *   Copyright (C) 2024 by Abderrahman Taha                                *
  *   taha_ab@yahoo.fr                                                      *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
