bf93146437412cadc1ff466073ab79e057748510
Abderrahman Taha <taha_ab@yahoo.fr>
2024-01-19 19:14:05 -0500

    #242 Add "Trefoil_Sweep" script

--- mathmodcollection.js
+++ mathmodcollection.js
@@ -1,96 +1,57 @@
 {
   "MathModels": [
-        {
-                    "Param3D": {
-                "Description": [
-                    "Trefoil_Sweep Creates a surface by sweeping a cross section curve along a spine curve"
-                ],
-        "Name": [
-            "Trefoil_Knot"
-        ],
-                        "Component": [
-                            "Knots"
-                        ],
-                        "Const": [
-                            "r=2",
-                            "du=1/1000000"
-                        ],
-                        "Funct": [
-                            "C1x=r*cos(v)",
-                            "C1y=r*sin(v)",
-                            "Cx=5*(sin(u) + 2*sin(2*u))",
-                            "Cy=5*(cos(u) - 2*cos(2*u))",
-                            "Cz=5*sin(3*u)",
-                            "Rapp=u/sqrt(u*u+v*v+t*t)",
-                            "dCxu=(Cx(u+du,v,t)-Cx(u,v,t))/du",
-                            "dCyu=(Cy(u+du,v,t)-Cy(u,v,t))/du",
-                            "dCzu=(Cz(u+du,v,t)-Cz(u,v,t))/du",
-                            "axu=Rapp(dCxu(u,v,t),dCyu(u,v,t),dCzu(u,v,t))",
-                            "ayu=Rapp(dCyu(u,v,t),dCzu(u,v,t),dCxu(u,v,t))",
-                            "azu=Rapp(dCzu(u,v,t),dCxu(u,v,t),dCyu(u,v,t))",
-                            "d2Cxu=(axu(u+du,v,t)-axu(u,v,t))/du",
-                            "d2Cyu=(ayu(u+du,v,t)-ayu(u,v,t))/du",
-                            "d2Czu=(azu(u+du,v,t)-azu(u,v,t))/du",
-                            "Ax=Rapp(d2Cxu(u,v,t),d2Cyu(u,v,t),d2Czu(u,v,t))",
-                            "Ay=Rapp(d2Cyu(u,v,t),d2Czu(u,v,t),d2Cxu(u,v,t))",
-                            "Az=Rapp(d2Czu(u,v,t),d2Cxu(u,v,t),d2Cyu(u,v,t))",
-                            "nx=(ayu(u,v,t)*Az(u,v,t)-azu(u,v,t)*Ay(u,v,t))",
-                            "ny=(azu(u,v,t)*Ax(u,v,t)-axu(u,v,t)*Az(u,v,t))",
-                            "nz=(axu(u,v,t)*Ay(u,v,t)-ayu(u,v,t)*Ax(u,v,t))",
-                            "Bx=Rapp(nx(u,v,t),ny(u,v,t),nz(u,v,t))",
-                            "By=Rapp(ny(u,v,t),nz(u,v,t),nx(u,v,t))",
-                            "Bz=Rapp(nz(u,v,t),nx(u,v,t),ny(u,v,t))",
-                            "S1x=Cx(u,v,t)+(C1x(u,v,t)*Bx(u,v,t)+C1y(u,v,t)*Ax(u,v,t))",
-                            "S1y=Cy(u,v,t)+(C1x(u,v,t)*By(u,v,t)+C1y(u,v,t)*Ay(u,v,t))",
-                            "S1z=Cz(u,v,t)+(C1x(u,v,t)*Bz(u,v,t)+C1y(u,v,t)*Az(u,v,t))"
-                        ],
-                        "Fx": [
-                            "S1x(u,v,t)"
-                        ],
-                        "Fy": [
-                            "S1y(u,v,t)"
-                        ],
-                        "Fz": [
-                            "S1z(u,v,t)"
-                        ],
-                        "Grid": [
-                            "400",
-                            "50"
-                        ],
-                        "Umax": [
-                            "2*pi"
-                        ],
-                        "Umin": [
-                            "0"
-                        ],
-                        "Vmax": [
-                            "2*pi"
-                        ],
-                        "Vmin": [
-                            "0"
-                        ]
-                    },
-                    "Sliders": {
-                "SetNames": [
-                    "Trefoil_Knot"
-                ],
-                        "Max": [
-                            "30"
-                        ],
-                        "Min": [
-                            "0"
-                        ],
-                        "Name": [
-                            "r"
-                        ],
-                        "Position": [
-                            "2"
-                        ],
-                        "Step": [
-                            "1"
-                        ]
-                    }
-                } ,
+    {
+        "Param3D": {
+        "Description":  ["Trefoil_Sweep Creates a surface by sweeping a cross section curve along a spine curve"],
+        "Name":  ["Trefoil_Knot"],
+            "Component":   ["Knots" ],
+            "Const":   ["r=2","du=1/1000000" ],
+            "Funct":   ["C1x=r*cos(v)",
+                "C1y=r*sin(v)",
+                "Cx=5*(sin(u) + 2*sin(2*u))",
+                "Cy=5*(cos(u) - 2*cos(2*u))",
+                "Cz=5*sin(3*u)",
+                "Rapp=u/sqrt(u*u+v*v+t*t)",
+                "dCxu=(Cx(u+du,v,t)-Cx(u,v,t))/du",
+                "dCyu=(Cy(u+du,v,t)-Cy(u,v,t))/du",
+                "dCzu=(Cz(u+du,v,t)-Cz(u,v,t))/du",
+                "axu=Rapp(dCxu(u,v,t),dCyu(u,v,t),dCzu(u,v,t))",
+                "ayu=Rapp(dCyu(u,v,t),dCzu(u,v,t),dCxu(u,v,t))",
+                "azu=Rapp(dCzu(u,v,t),dCxu(u,v,t),dCyu(u,v,t))",
+                "d2Cxu=(axu(u+du,v,t)-axu(u,v,t))/du",
+                "d2Cyu=(ayu(u+du,v,t)-ayu(u,v,t))/du",
+                "d2Czu=(azu(u+du,v,t)-azu(u,v,t))/du",
+                "Ax=Rapp(d2Cxu(u,v,t),d2Cyu(u,v,t),d2Czu(u,v,t))",
+                "Ay=Rapp(d2Cyu(u,v,t),d2Czu(u,v,t),d2Cxu(u,v,t))",
+                "Az=Rapp(d2Czu(u,v,t),d2Cxu(u,v,t),d2Cyu(u,v,t))",
+                "nx=(ayu(u,v,t)*Az(u,v,t)-azu(u,v,t)*Ay(u,v,t))",
+                "ny=(azu(u,v,t)*Ax(u,v,t)-axu(u,v,t)*Az(u,v,t))",
+                "nz=(axu(u,v,t)*Ay(u,v,t)-ayu(u,v,t)*Ax(u,v,t))",
+                "Bx=Rapp(nx(u,v,t),ny(u,v,t),nz(u,v,t))",
+                "By=Rapp(ny(u,v,t),nz(u,v,t),nx(u,v,t))",
+                "Bz=Rapp(nz(u,v,t),nx(u,v,t),ny(u,v,t))",
+                "S1x=Cx(u,v,t)+(C1x(u,v,t)*Bx(u,v,t)+C1y(u,v,t)*Ax(u,v,t))",
+                "S1y=Cy(u,v,t)+(C1x(u,v,t)*By(u,v,t)+C1y(u,v,t)*Ay(u,v,t))",
+                "S1z=Cz(u,v,t)+(C1x(u,v,t)*Bz(u,v,t)+C1y(u,v,t)*Az(u,v,t))" ],
+            "Fx":   ["S1x(u,v,t)" ],
+            "Fy":   ["S1y(u,v,t)" ],
+            "Fz":   ["S1z(u,v,t)" ],
+            "Grid":   ["400","50" ],
+            "Umax":   ["2*pi" ],
+            "Umin":   ["0" ],
+            "Vmax":   ["2*pi" ],
+            "Vmin":   ["0"
+            ]
+        },
+        "Sliders": {
+        "SetNames":  ["Trefoil_Knot"],
+            "Max":   ["30"],
+            "Min":   ["0"],
+            "Name":   ["r"],
+            "Position":   ["2"],
+            "Step":   ["1"]
+        }
+    } ,
     {
         "Iso3D": {
             "Description": ["Ukraine Heart Flag by Abderrahman Taha 24/02/2022"],
