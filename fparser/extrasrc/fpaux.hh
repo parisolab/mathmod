bc875af706175cac6bbf0a1ce9f5f0110fc9a900
Abderrahman Taha <taha_ab@yahoo.fr>
2024-09-20 11:23:43 -0400

    #245 MathMod for Android LLVM/Clang compilation error

--- fparser/extrasrc/fpaux.hh
+++ fparser/extrasrc/fpaux.hh
@@ -853,10 +853,23 @@ namespace FUNCTIONPARSERTYPES
     {
         if(value.imag() == T()) return os << value.real();
         if(value.real() == T()) return os << value.imag() << 'i';
+        /*
         if(value.imag() < T())
             return os << '(' << value.real() << "-" << -value.imag() << "i)";
         else
             return os << '(' << value.real() << "+" << value.imag() << "i)";
+        */
+        if(value.imag() < T())
+        {
+            std::string str = '(' + value.real() +  "-" + (-value.imag()) + "i)";
+            return os << str;
+        }
+        else
+        {
+            std::string str = '(' + value.real() +  "+" + value.imag() + "i)";
+            return os << str;
+        }
+
     }
 
     /* Less-than or greater-than operators are not technically defined
