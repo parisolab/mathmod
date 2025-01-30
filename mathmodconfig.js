04e9b4b03a3440a70e24ca122c9f7cfd8657367c
Abderrahman Taha <taha_ab@yahoo.fr>
2025-01-29 21:06:50 -0500

    #180 Code cleaning

--- mathmodconfig.js
+++ mathmodconfig.js
@@ -5,12 +5,7 @@
         "VersionNumber": "1200",
         "ReleaseDateYMD": "250220",
         "DocAbsolutePath": "https://github.com/parisolab/mathmod/wiki/documentation",
-        "DocPartialPath": "../Resources/documentation/index.html",
-
-        "_DocPartialPath": "../Resources/documentation/index.html",
-        "_DocAbsolutePath": "https://github.com/parisolab/mathmod/wiki/documentation",
-        "_FileConfig": "Resources/mathmodconfig.js",
-        "_FileCollection": "Resources/mathmodcollection.js"
+        "DocPartialPath": "../Resources/documentation/index.html"
     },
     "Parameters": {
         "AutomaticInitGrid": false,
