a0d5c61d9729c5b9decc935636acc0c35fe791d2
Abderrahman Taha <taha_ab@yahoo.fr>
2025-08-07 09:19:04 -0400

    #180 Code cleaning

--- ui_modules/drawingoptions.cpp
+++ ui_modules/drawingoptions.cpp
@@ -5631,16 +5631,13 @@ void DrawingOptions::on_SaveThButton_2_clicked()
     ConstArray.append("ShowUpperSurf_"+QString::number(ThCount)+"="+Bool);
     Bool = ((MathmodRef->IsoObjet->IsoTh.ShowOriginalSurf) ? "1" : "0");
     ConstArray.append("ShowOriginalSurf_"+QString::number(ThCount)+"="+Bool);
-
     tmpScalVar    = "ScalVar_"+QString::number(ThCount);
     tmpScalVarmax = "ScalVarMax_"+QString::number(ThCount);
     tmpScalVarmin = "ScalVarMin_"+QString::number(ThCount);
     ConstArray.append(tmpScalVar+"="+QString::number(ui.SscrollBar->value()));
     ConstArray.append(tmpScalVarmax+"="+QString::number(ui.SscrollBar->maximum()));
     ConstArray.append(tmpScalVarmin+"="+QString::number(ui.SscrollBar->minimum()));
-
     ScalVar = "(("+tmpScalVar+"-("+tmpScalVarmax+"-"+tmpScalVarmin+")/2)/10)";
-
     if(ThCount==1)
     {
         ConstArray.append("epsilon=1/100000");
