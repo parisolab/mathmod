ddf9239f10fb0e07603297c06a23b7adeae8e323
Abderrahman Taha <taha_ab@yahoo.fr>
2025-11-13 19:51:40 -0500

    #289 code optimization

--- ui_modules/mathmod.cpp
+++ ui_modules/mathmod.cpp
@@ -1665,7 +1665,7 @@ void MathMod::CopyData(ObjectProperties *scene)
     {
         glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo[1]);
-        if(scene->VertxNumber>previousVertxNumber>0)
+        if((scene->VertxNumber>previousVertxNumber) && (scene->VertxNumber>0))
         {
             glBufferData(GL_ARRAY_BUFFER, sizeof(GL_FLOAT)*10*(scene->VertxNumber+(12+60+36)), scene->ArrayNorVer_localPt, GL_STATIC_DRAW);
             previousVertxNumber = scene->VertxNumber;
@@ -1676,7 +1676,8 @@ void MathMod::CopyData(ObjectProperties *scene)
             previousVertxNumber = scene->VertxNumber;
         }
 
-        if((scene->PolyNumber + scene->NbPolygnNbVertexPtMinSize)>previousPolyNumberNbPolygnNbVertexPtMin)
+        if(((scene->PolyNumber + scene->NbPolygnNbVertexPtMinSize)>previousPolyNumberNbPolygnNbVertexPtMin) &&
+            ((scene->PolyNumber + scene->NbPolygnNbVertexPtMinSize)>0))
         {
             glBufferData(GL_ELEMENT_ARRAY_BUFFER,sizeof(GLuint)*(scene->PolyNumber + scene->NbPolygnNbVertexPtMinSize), scene->PolyIndices_localPt, GL_STATIC_DRAW);
             previousPolyNumberNbPolygnNbVertexPtMin =  (scene->PolyNumber + scene->NbPolygnNbVertexPtMinSize);
