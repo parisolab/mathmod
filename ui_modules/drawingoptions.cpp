826c45fac5be5e14b10d9ebeb3f32d64e1f427b1
Abderrahman Taha <taha_ab@yahoo.fr>
2026-02-04 19:35:36 -0500

    #299 Add possibility to limit an operation (from the equation generator)

--- ui_modules/drawingoptions.cpp
+++ ui_modules/drawingoptions.cpp
@@ -5622,14 +5622,9 @@ void DrawingOptions::ApplyThiIsoOperation(QJsonObject & OriginalObj, QJsonArray 
                     "*psh((1),(fffxyz"+I+"(x,y+epsilon,z,t)-fffxyz"+I+"(x,y,z,t))/epsilon)"
                     "*psh((2),(fffxyz"+I+"(x,y,z+epsilon,t)-fffxyz"+I+"(x,y,z,t))/epsilon)"
                     "*psh((3),("+ScalVar+"*ThExpression_"+QString::number(ThCount)+"(x,y,z,t)/sqrt(csd(0)*csd(0)+ csd(1)*csd(1)+ csd(2)*csd(2))))");
-        fct_opt+= ShowOriginalSurfStr;
-        fct_opt+= ShowUpperSurfStr;
-        fct_opt+= ShowBottomSurfStr;
+        fct_opt+= ShowOriginalSurfStr+ShowUpperSurfStr+ShowBottomSurfStr;
 
-        QString fct_raw="fffxyz_raw"+I+"=";
-        fct_raw+= ShowOriginalSurfRawStr;
-        fct_raw+= ShowUpperSurfRawStr;
-        fct_raw+= ShowBottomSurfRawStr;
+        QString fct_raw="fffxyz_raw"+I+"="+ShowOriginalSurfRawStr+ShowUpperSurfRawStr+ShowBottomSurfRawStr;
 
         QString fct="fffxyz"+I+"= if(RawScript_"+QString::number(ThCount)+"=(1), fffxyz_raw"+I+"(x,y,z,t), fffxyz_opt"+I+"(x,y,z,t))";
 
